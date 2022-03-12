a705babac6e62eb99e197374dd259d29d98288f8
SurDin <surdin@gmail.com>
2022-03-12 11:52:26 +0200

    refactor styling

--- drawingproj/Movie Database.js
+++ drawingproj/Movie Database.js
@@ -1,267 +1,38 @@
-movies= {
-    1: {movNam : 'If I Stay',
-        movtra : 'rMp896hfp74',
-        imdbID : 'tt1355630',
-        rating : 'PG-13',
-        streamSub :['Prime'],
-        rent : ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/ifIStay.jpg'},
-
-    2:  {movNam: 'Hook',
-        movtra: 'c-vwgt8cwEM',
-        imdbID: 'tt0102057',
-        rating: 'PG',
-        streamSub: ['None'],
-        rent :['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/hook.jpg'},
-
-    3:  {movNam: 'Bridge in the river Kwai',
-        movtra: 'RlC7XBayj0s',
-        imdbID: 'tt0050212',
-        rating: 'PG (1957)',
-        streamSub: ['None'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/bridgeKwai.jpg'},
-
-    4: {movNam: 'Willow',
-        movtra: 'uzn2izehkno&t=',
-        imdbID: 'tt0096446',
-        rating: 'PG',
-        streamSub: ['Disney+'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/willow.jpg'},
-
-    5: {movNam : 'JoJo Rabbit',
-        movtra : 'tL4McUzXfFI&t=',
-        imdbID : 'tt2584384',
-        rating : 'PG-13',
-        streamSub: ['None'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/jojoRabbit.jpg'},
-
-    6: {movNam: 'Secondhand Lions',
-        movtra: 'Ebkrm7u44UI&t=',
-        imdbID: 'tt0327137',
-        rating: 'PG',
-        streamSub: ['None'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/secondhandLions.jpg'},
-
-    7: {movNam: 'Young Sherlock Holmes',
-        movtra: 'PVTHS6vyOZw',
-        imdbID: 'tt0090357',
-        rating: 'PG-13',
-        streamSub: ['Paramount+'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/youngSherHolmes.jpg'},
-
-    8: {movNam: "The King's Speech",
-        movtra: 'EcxBrTvLbBM&t=',
-        imdbID: '5',
-        rating: 'R',
-        streamSub: ['None'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/kingsSpeech.jpg'},
-
-    9: {movNam: 'Game Night'
-        ,movtra: 'qmxMAdV6s4U&t='
-        ,imdbID: 'tt2704998'
-        ,rating: 'R',   
-        streamSub: ['None'],
-        rent: ['Itunes','Amazon','Vudu'],
-        image : 'images/gameNight.jpg'},
-
-    10: {movNam: 'The Village'
-        ,movtra: 'sTGyhwvdY6k&t='
-        ,imdbID: 'tt0368447'
-        ,rating: 'PG-13'
-        ,streamSub: ['Prime'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/theVillage.jpg'},
-
-    11: {movNam: 'Hugo'
-        ,movtra: 'FG9rvNhdOX8&t='
-        ,imdbID: 'tt0970179'
-        ,rating: 'PG'
-        ,streamSub: ['None'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/hugo.jpg'},
-
-    12: {movNam : 'The Private Eyes',
-        movtra : 'MCOQP_LaWtQ&t=',
-        imdbID : 'tt0081376',
-        rating : 'PG (1980)',
-        streamSub: ['None'],
-        rent: ['None'],
-        image : 'images/privateEyes.jpg'},
-
-    13: {movNam : 'Treasure Planet',
-        movtra : 'IVohgBUMCT0&t=',
-        imdbID : 'tt0133240',
-        rating : 'PG',
-        streamSub: ['Disney+'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/treasurePlanet.jpg'},
-
-    14: {movNam : 'Arrival',
-        movtra : 'tFMo3UJ4B4g&t=',
-        imdbID : 'tt2543164',
-        rating : 'PG-13',
-        streamSub: ['Hulu', 'Paramont+'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/arrival.jpg'},
-
-    15: {movNam : 'The Terminal',
-        movtra : 'iZqQRmhRvyg&t',
-        imdbID : 'tt0362227',
-        rating : 'PG-13',
-        streamSub: ['None'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/terminal.jpg'},
-
-    16: {movNam : 'Mulan 1998',
-        movtra : 'HKH7_n425Ss',
-        imdbID : 'tt0120762',
-        rating : 'G',
-        streamSub: ['Disney+'],
-        rent: ['Itunes','Amazon','Vudu'],
-        image : 'images/mulan.jpg'},
-
-    17: {movNam  : 'Catch me if you can',
-        movtra  : 's-7pyIxz8Qg',
-        imdbID  : 'tt0264464',
-        rating : 'PG-13',
-        streamSub: ['None'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/catchMeIfYouCan.jpg'},
-
-    18: {movNam  : 'Princsess Mononoke',
-        movtra  : '4OiMOHRDs14',
-        imdbID  : 'tt0119698',
-        rating  : 'PG-13',
-        streamSub: ['HBOMax'],
-        rent: ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/princsessMononoke.jpg'},
-        
-    19: {movNam : 'The Pelican Brief',
-        movtra : 'N7seUNbY1_w',
-        imdbID : 'tt0107798',
-        rating : 'PG-13',
-        streamSub: ['HBOMax'],
-        rent : ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/pelicanBrief.jpg'},
-
-    20: {movNam : 'Master and Commander',
-        movtra : 'tHTHCNYiiHk&t',
-        imdbID : 'tt0311113',
-        rating : 'PG-13',
-        streamSub: ['None'],
-        rent : ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/masterAndCommander.jpg'},
-
-    21: {movNam : 'To The Bone',
-        movtra : '705yRfs6Dbs&t',
-        imdbID : 'tt5541240',
-        rating : 'TV-MA (R)',
-        streamSub: ['Netflix'],
-        rent : ['None'],
-        image :'images/toTheBone.jpg'},
-
-    22:{movNam : 'Rear Window',
-        movtra : 'm01YktiEZCw',
-        imdbID : 'tt0047396',
-        rating : 'PG (1954)',
-        streamSub:['None'],
-        rent : ['Youtube','Itunes','Amazon','Vudu'],
-        image : 'images/rearWindow.jpg'
-    },
-
-    23:{movNam : 'Toy Story 4',
-        movtra : 'wmiIUN-7qhE',
-        imdbID : 'tt1979376',
-        rating : 'G',
-        streamSub : ['Disney+'],
-        rent : ['Itunes','Amazon','Vudu'],
-        image : 'images/toyStory4.jpg'
-    },
-
-    24:{movNam : 'Fargo',
-        movtra : 'h2tY82z3xXU',
-        imdbID : 'tt0116282',
-        rating : 'R',
-        streamSub : ['Prime'],
-        rent : ['Itunes','Amazon','Vudu'],
-        image : 'images/fargo'
-    },
-  }
-var movNum = Object.keys(movies)
-var randomNum;
-function draMovie (max){
-    max = movNum.length
-    randomNum=Math.floor(Math.random() * max + 1);
-    return randomNum
+async function getResultTemplate() {
+    return fetch("result.html")
+	    .then(response => response.text());
 }
 
-function draw (){
-    movRes1=draMovie()
-    document.querySelector("#mov1-1").innerHTML = '#'+randomNum+' **'+movies[movRes1]["movNam"]+'** '
-    document.querySelector("#mov1-2").innerHTML = '> Rated: '+movies[movRes1]["rating"]
-    document.querySelector("#mov1-3").innerHTML = '> Content Guide: '
-    document.querySelector("#mov1-4").innerHTML = '> https://www.imdb.com/title/'+movies[movRes1]["imdbID"]
-    document.querySelector("#mov1-5").innerHTML = "> Trailer: "
-    document.querySelector("#mov1-6").innerHTML = '> https://www.youtube.com/watch?v='+movies[movRes1]['movtra']
-    image1=movies[movRes1]['image']
-    movRes2=draMovie()
-    document.querySelector("#mov2-1").innerHTML = '#'+randomNum+' **'+movies[movRes2]["movNam"]+'** '
-    document.querySelector("#mov2-2").innerHTML = '> Rated: '+movies[movRes2]["rating"]
-    document.querySelector("#mov2-3").innerHTML = '> Content Guide: '
-    document.querySelector("#mov2-4").innerHTML = '> https://www.imdb.com/title/'+movies[movRes2]["imdbID"]
-    document.querySelector("#mov2-5").innerHTML = "> Trailer: "
-    document.querySelector("#mov2-6").innerHTML = '> https://www.youtube.com/watch?v='+movies[movRes2]['movtra']
-    image2=movies[movRes2]['image']
-    movRes3=draMovie()
-    document.querySelector("#mov3-1").innerHTML = '#'+randomNum+' **'+movies[movRes3]["movNam"]+'** '
-    document.querySelector("#mov3-2").innerHTML = '> Rated: '+movies[movRes3]["rating"]
-    document.querySelector("#mov3-3").innerHTML = '> Content Guide: '
-    document.querySelector("#mov3-4").innerHTML = '> https://www.imdb.com/title/'+movies[movRes3]["imdbID"]
-    document.querySelector("#mov3-5").innerHTML = "> Trailer: "
-    document.querySelector("#mov3-6").innerHTML = '> https://www.youtube.com/watch?v='+movies[movRes3]['movtra']
-    image3=movies[movRes3]['image']
-    return movRes1, movRes2, movRes3,image1,image2,image3
+async function getMovies() {
+    return fetch("movies.json")
+	    .then(response => response.json());
 }
-draw()
-var img = document.getElementById("#movimage1");
-img.src = 'image1'
-//console.log(movRes1)
-/*print('#' + lotnum, ' **' + movnam + '**'
-'\n> Rated', rating, ''
-'\n> Content Guide: '
-'\n> https://www.imdb.com/title/' + imdbid + '/parentalguide',
-'\n> Trailer: '
-'\n> https://www.youtube.com/watch?v=' + movtra)*/
-/*14: {movNam : 'The Changling',
-        movtra : 'xTzgXVosQOU&t=',
-        imdbID : 'tt0080516',
-        rating : 'R (1980)',
-        streamSub: ['None'],
-        rent: ['Youtube','Itunes','Amazon','Vudu']},
-10: {movNam: 'Joker'
-        ,movtra: 't433PEQGErc&t='
-        ,imdbID:'tt7286456'
-        ,rating: 'R',
-        streamSub: ['HBOMax'],
-        rent: ['Itunes','Amazon','Vudu']},
-4:  {movNam: 'The Neverending Story',
-        movtra: 'UeFni9dOv7c',
-        imdbID: 'tt0088323',
-        rating: 'PG',
-        streamSub: ['Netflix'],
-        rent: ['Youtube','Itunes','Amazon','Vudu']},*/
-//add movie template
-/*24:{movNam : '@',
-        movtra : '@',
-        imdbID : '@',
-        rating : '@',
-        streamSub : ['@'],
-        rent : ['@'],
-        image : 'image/@'},*/
+
+function drawMovie(movies){
+    return Math.floor(Math.random() * movies.length);
+}
+
+function formatTemplate(movies, choice, result_template) {
+	movie = movies[choice];
+	result = result_template.replace("{number}", choice)
+				.replace("{name}", movie["movNam"])
+	            .replace("{rating}", movie["rating"])
+				.replace("{imbd_id}", movie["imbdID"])
+				.replace("{youtube_id}", movie["movtra"])
+				.replace("{image_link}", movie['image']);
+	console.log(result);
+	return result;	
+}
+
+async function draw (){
+	movies = await getMovies();
+	result_template = await getResultTemplate();
+
+	for(let i = 0; i < 3; i++) {
+		choice = drawMovie(movies);
+		result = formatTemplate(movies, choice, result_template);
+		document.querySelector("div.results").innerHTML += result;
+	}
+}
+
+(async() => {draw()})();
